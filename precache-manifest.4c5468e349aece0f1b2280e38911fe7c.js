678f041f581bd57e3d59e7f41dd06f3471eaef84
GeoQer <40548603+GeoQer@users.noreply.github.com>
2019-01-22 18:43:31 -0700

    Updates

---
+++ precache-manifest.4c5468e349aece0f1b2280e38911fe7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "73861b391c8a4afd69f8",
+    "url": "/clickygame/static/js/main.73861b39.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/clickygame/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "73861b391c8a4afd69f8",
+    "url": "/clickygame/static/css/main.5cc54038.chunk.css"
+  },
+  {
+    "revision": "6ed20c9a5aebdc177d8b1169dfc506c9",
+    "url": "/clickygame/index.html"
+  }
+];
