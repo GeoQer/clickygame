96f5359612f1c8ec100518f4e9d1b82e560f031e
GeoQer <40548603+GeoQer@users.noreply.github.com>
2019-01-22 17:59:03 -0700

    BackDrop typo fixed. Click now works.

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -6,7 +6,7 @@ const BackDrop = function (props) {
     return (
         <div className="row" onClick={props.ClickHandler}>
             {props.images.map((image, i) => {
-            return <Image key={props.images[i].name} src={props.images[i].src} alt={props.images[i].name} onClick={props.onCLick} />})}
+            return <Image key={props.images[i].name} src={props.images[i].src} alt={props.images[i].name} onClick={props.onClick} />})}
         </div>
     )
 }
